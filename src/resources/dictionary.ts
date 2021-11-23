8ab7a88c01baeef9248632ce9c1554afa23e8193
ipez <ipez@users.noreply.github.com>
2021-11-23 21:52:49 +0800

    Update dictionary.ts

--- src/resources/dictionary.ts
+++ src/resources/dictionary.ts
@@ -14,7 +14,6 @@ export type Dictionary = {
 export const dictionaries: Dictionary[] = [
   
   { id: 'cet4', name: 'CET-4', description: '大学英语四级词库', category: '英语学习', url: '', length: 2607 },
-  { id: 'scb', name: '生词本', description: '大学英语四级词库', category: '英语学习', url: './dicts/shengciben.json', length: 2600},
   { id: 'cet6', name: 'CET-6', description: '大学英语六级词库', category: '英语学习', url: './dicts/CET6_T.json', length: 2345 },
   { id: 'gmat', name: 'GMAT', description: 'GMAT 词库', category: '英语学习', url: './dicts/GMAT_3_T.json', length: 3047 },
   { id: 'gre', name: 'GRE', description: 'GRE 词库', category: '英语学习', url: './dicts/GRE_3_T.json', length: 6515 },
